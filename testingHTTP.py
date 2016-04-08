daacc62465db5557dfc8ba5a33ec31d671a34b67
John Moss <john@stolen-bikes.co.uk>
2016-04-08 12:51:34 +0100

    Create testingHTTP.py

---
+++ testingHTTP.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+import sys, os, argparse, urllib2
+from urllib2 import Request
+#from urllib.request import urlopen
+from multiprocessing import Pool
+
+def findftp(domain):
+	domain = domain.strip()
+    
+	# TAKE A LOOK FOR robots.txt file
+	# Try to download http://target.tld/robots.txt
+	request = Request('http://' + domain + "/robots.txt")
+	req = urllib2.urlopen(request)
+	answer = req.read()
+
+	# Write match to OUTPUTFILE
+  print "done"
+	return
+    
+domains = findftp("google.com");
