8c2e8fa489cd04a554119682e0cf22a4e33137df
John Moss <john@stolen-bikes.co.uk>
2016-09-22 11:51:53 +0100

    Update robotSpider.py

--- robotSpider.py
+++ robotSpider.py
@@ -4,6 +4,64 @@ from urllib2 import Request
 #from urllib.request import urlopen
 from multiprocessing import Pool
 
+def process(domain, req, answer):
+	print("[*] Found robots: " + domain + " :: " + str(req.code))
+	
+	# Lets process the output
+	
+	## General File Statistics
+	responseLines = len(answer.split('\n'))
+	responseCharacters = len(answer)
+	
+	## UserAgents
+	responseUseragents = answer.count("User-agent")
+	
+	## Sitemaps
+	responseSitemaps = answer.count("Sitemap")
+	
+	## Disallows and Allows
+	responseAllows = answer.count("Allow")
+	responseDisallows = answer.count("Disallow")
+	
+	# Write match to OUTPUTFILE
+	fHandle = open(SUMMARYFILE,'a')
+	#domain, file, response, lines, characters, useragents, sitemaps, allows, disallows
+	fHandle.write(domain + ", robots.txt, " + str(req.code) + ", " + str(responseLines) + ", " + str(responseCharacters) + ", " + str(responseUseragents) + ", " + str(responseSitemaps) + ", " + str(responseAllows) + ", " + str(responseDisallows) + "\n" )
+	fHandle.close()
+	
+	#Process line by line now
+	responseLines = answer.split('\n')
+	for responseLine in responseLines:
+		#print responseLine
+		if "user-agent:" in responseLine.lower():
+			useragent = responseLine.lower().replace("user-agent:", "").strip()
+	                # Write match to OUTPUTFILE
+        		fHandle = open(USERAGENTFILE, 'a')
+	                fHandle.write(domain + ", " + useragent + "\n")
+       			fHandle.close()
+			#print useragent
+		elif "disallow:" in responseLine.lower():
+			type = "disallow"
+			directory = responseLine.lower().replace("disallow:", "").strip()
+                        # Write match to OUTPUTFILE
+                        fHandle = open(RULESFILE, 'a')
+                        fHandle.write(domain + ", " + useragent + ", " + type + ", "+ directory + "\n")
+                        fHandle.close()
+                elif "allow:" in responseLine.lower():
+                        type = "allow"
+                        directory = responseLine.lower().replace("allow:", "").strip()
+                        # Write match to OUTPUTFILE
+                        fHandle = open(RULESFILE, 'a')
+                        fHandle.write(domain + ", " + useragent + ", " + type + ", "+ directory + "\n")
+                        fHandle.close()
+                elif "sitemap:" in responseLine.lower():
+                        sitemap = responseLine.lower().replace("sitemap:", "").strip()
+                        # Write match to OUTPUTFILE
+                        fHandle = open(SITEMAPFILE, 'a')
+                        fHandle.write(domain + ", " + sitemap + "\n")
+                        fHandle.close()
+	return
+
 def findftp(domain):
 	domain = domain.strip()
     
@@ -14,63 +72,8 @@ def findftp(domain):
 		request = Request('http://' + domain + "/robots.txt", None, headers)
 		req = urllib2.urlopen(request)
 		answer = req.read()
-				
-		print("[*] Found robots: " + domain + " :: " + str(req.code))
+		process(domain, req, answer)
 		
-		# Lets process the output
-		
-		## General File Statistics
-		responseLines = len(answer.split('\n'))
-		responseCharacters = len(answer)
-		
-		## UserAgents
-		responseUseragents = answer.count("User-agent")
-		
-		## Sitemaps
-		responseSitemaps = answer.count("Sitemap")
-		
-		## Disallows and Allows
-		responseAllows = answer.count("Allow")
-		responseDisallows = answer.count("Disallow")
-		
-		# Write match to OUTPUTFILE
-		fHandle = open(SUMMARYFILE,'a')
-		#domain, file, response, lines, characters, useragents, sitemaps, allows, disallows
-		fHandle.write(domain + ", robots.txt, " + str(req.code) + ", " + str(responseLines) + ", " + str(responseCharacters) + ", " + str(responseUseragents) + ", " + str(responseSitemaps) + ", " + str(responseAllows) + ", " + str(responseDisallows) + "\n" )
-		fHandle.close()
-		
-		#Process line by line now
-		responseLines = answer.split('\n')
-		for responseLine in responseLines:
-			#print responseLine
-			if "user-agent:" in responseLine.lower():
-				useragent = responseLine.lower().replace("user-agent:", "").strip()
-		                # Write match to OUTPUTFILE
-                		fHandle = open(USERAGENTFILE, 'a')
-		                fHandle.write(domain + ", " + useragent + "\n")
-               			fHandle.close()
-				#print useragent
-			elif "disallow:" in responseLine.lower():
-				type = "disallow"
-				directory = responseLine.lower().replace("disallow:", "").strip()
-                                # Write match to OUTPUTFILE
-                                fHandle = open(RULESFILE, 'a')
-                                fHandle.write(domain + ", " + useragent + ", " + type + ", "+ directory + "\n")
-                                fHandle.close()
-                        elif "allow:" in responseLine.lower():
-                                type = "allow"
-                                directory = responseLine.lower().replace("allow:", "").strip()
-                                # Write match to OUTPUTFILE
-                                fHandle = open(RULESFILE, 'a')
-                                fHandle.write(domain + ", " + useragent + ", " + type + ", "+ directory + "\n")
-                                fHandle.close()
-                        elif "sitemap:" in responseLine.lower():
-                                sitemap = responseLine.lower().replace("sitemap:", "").strip()
-                                # Write match to OUTPUTFILE
-                                fHandle = open(SITEMAPFILE, 'a')
-                                fHandle.write(domain + ", " + sitemap + "\n")
-                                fHandle.close()
-
 		return
 
 	except Exception as e:   
